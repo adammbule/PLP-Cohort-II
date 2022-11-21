c9b82be4893efbef546e828470a932f621e8ec11
unknown <adammbule98@gmail.com>
2022-11-22 00:10:56 +0300

    First of Many

---
+++ Assignments/PythonTask.py
@@ -0,0 +1,53 @@
+#Adam Mbule Barasa adammbule98@gmail.com Assignment submission date:20/11/2022
+#The assignment comprises of thee questions.
+
+#In each question, you are required to write python code that displays the expected output.
+
+#You are expected to share your source code for review.
+
+
+#Question one
+
+#Write a python program that will display the number and its square in the following format.
+
+#Number              Square
+
+#1                             1
+
+#2                             4
+
+#3                             9
+
+#4                             14
+
+#5                             25
+
+#6                             36
+
+#Question two
+
+#Write a Python program that prints the length of a string.
+
+#For example of expected results;
+
+#Input Output Explanation
+
+#"" 0 0 because its empty string
+
+#“Jambo” 5, 5 because it has 5 characters
+
+#“Power Learn Project (PLP)” ? ?
+
+ 
+
+#Question Three
+
+#Write a Python program that does the following:
+
+#Prints the character at index i in the string "Live happily ".
+
+#my_string = "Live happily ".
+
+#If the index is out of range, the program should print "i is out of range"
+
+#If the string is empty, the program should print "Empty String"
---
+++ KSaruniFunction.py
@@ -0,0 +1,9 @@
+l_name=int()
+def (f_name, l_name): #positional parameters
+    print(f"Hello, {f_name} {l_name}. Welcome to PLP")
+
+    print()
+
+quickfix
+#user defined functions
+#built in functions
