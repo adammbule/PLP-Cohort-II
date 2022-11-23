eb5866927d64cf79453c484b2ae8db506d2c0935
unknown <adammbule98@gmail.com>
2022-11-23 20:23:46 +0300

    Dart First Class Wueh

---
+++ LenkaaFirstDart.dart
@@ -0,0 +1,9 @@
+void main() {
+    list<string> countries = ["Kenya", "Tanzania", "Zambia", "South Africa"];
+    countries.addAll(["Ethiopia", "Nigeria"]);
+    print(countries);
+}
+
+void countryinAfrica(list){
+
+}
