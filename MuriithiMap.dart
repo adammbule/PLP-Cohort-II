daffb449ac1b5f4b554b493e4215681292e05de5
unknown <adammbule98@gmail.com>
2022-11-24 18:57:42 +0300

    First Dart Class

---
+++ MuriithiMap.dart
@@ -0,0 +1,40 @@
+void main() {
+  map<string, string> myDetails = {
+    "name": "Eric",
+    "address": "Nairobi",
+    "favoriteSport": "football",
+    "favoriteColor": "blue"
+  };
+  print(myDetails);
+}
+//Maps are key values that are stored in pairs
+
+//Function that increases anumber by 2. print the output
+void main() {}
+
+//Find the sum of two characters
+void sumTwoNumber(Number1, Number2) {
+  int results;
+  results = Number1 + Number2;
+  print(results);
+}
+
+void main() {
+  sumTwoNumber(8, 10);
+}
+
+//You can Only vote if your ageis over 18
+void VoterValidation(Age) {
+  if (Age >= 18) {
+    print("You are allowed to vote!!!");
+  } else {
+    print("You are still young to vote.");
+  }
+}
+
+void VoterValidation(Age) {
+  Age(18);
+}
+
+//Declare constant type of int and set it to 7
+const int age = 7; //eg declare pi=3.142
