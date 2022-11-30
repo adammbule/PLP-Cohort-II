a88ab953b986950103df46440b1eaee27a3c6de2
unknown <adammbule98@gmail.com>
2022-11-30 21:07:37 +0300

    Dart Installed Successfully

---
+++ LenkaaNov.dart
@@ -0,0 +1,27 @@
+// program to remove white spaces
+void main() {
+  String name = "COCA COLA";
+  print(name.replaceAll(' ', ''));
+}
+
+///Sth to do with dates....control flows???
+void main() {
+  //print numbers from 0 to 5
+  for (int i = 0; i <= 5; i++) {
+    print(i);
+  }
+} //will give out 0 1 2 3 4 5 Exited
+
+void main() {
+  //print numbers from 0-5
+  num count = 0;
+  while (count <= 20) {
+    print(count);
+    count++;
+  }
+}
+
+//labels with break statements
+
+
+
