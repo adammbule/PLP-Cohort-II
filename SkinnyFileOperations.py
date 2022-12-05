cd5ad1742c49349feb6254565b18806c2065e33a
unknown <adammbule98@gmail.com>
2022-12-05 23:55:48 +0300

    Getting there on JS

--- SkinnyFileOperations.py
+++ SkinnyFileOperations.py
@@ -7,3 +7,20 @@ f = open("Test.txt", mode='r', encoding='UTF-8')
 f.close()
 with open("Test.txt", 'w', encoding='utf-8') as f:
     f.write("Text testing\n")
+    f.write("This is my second try.Wow!!\n")
+    f.write("She makes me so happy like coffee!!")
+f = open("Test.txt", 'r', encoding='utf-8')
+f.read(5)
+f.read(10)
+f.read()
+
+#Lambda function same as def function 
+# Research on use cases of both functions
+greet_user = lambda : print("Hello Mbule")
+greet_user() #call the function
+
+#lambdafunction with an argument
+list_num = [1,2,3,4,5,6,7,8,9]
+filt = list(filter(lambda x: x%2==0, list_num))
+
+print(filt)
---
+++ mutukuJavascript.js
@@ -0,0 +1,22 @@
+// This is a human Value
+let name = "Adam Mbule"
+var age = 25
+const gender = "Male"
+let programmer = true
+let experience = "1 Year"
+let status = undefined
+
+console.log("Hello World!!")
+console.log(gender)
+console.log(name, age, programmer)
+const person = {
+    height: 67,
+    weight: 60,
+}
+
+console.log(person)
+console.log(person.height)
+
+const students = ["Adam", "Mbule", "Barasa","Anifa"]
+console.log(students)
+console.log(students[2])
